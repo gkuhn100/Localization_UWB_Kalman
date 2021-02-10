3a8486f75abb4a5960d8c589b526f1137defa597
gkuhn100 <55503972+gkuhn100@users.noreply.github.com>
2021-02-09 20:11:24 -0600

    Update Kalman.c

--- Kalman.c
+++ Kalman.c
@@ -12,6 +12,27 @@
 
 #include "dwm_api.h"
 #include "hal.h"
+#include <wiringPiI2C.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define Device_Address 0x68 /*Device Address Identifier for MPU6050*/
+
+
+#define PWR_MGMT_1   0x6B 
+#define SMPLRT_DIV   0x19 
+#define CONFIG       0x1A
+#define GYRO_CONFIG  0x1B
+#define INT_ENABLE   0x38
+#define ACCEL_XOUT_H 0x3B
+#define ACCEL_YOUT_H 0x3D
+#define ACCEL_ZOUT_H 0x3F
+#define GYRO_XOUT_H  0x43
+#define GYRO_YOUT_H  0x45
+#define GYRO_ZOUT_H  0x47
+
+
 
 int main(void)
 {
